78d792f2c9b3ff688efc6893682bdb48688461c9
gustavo <gusgurtavo@gmail.com>
2023-12-06 12:05:16 -0300

    ajuste

--- app/crud.py
+++ app/crud.py
@@ -4,13 +4,13 @@ from . import schemas
 from sqlalchemy.orm import Session
 
 
-
-
 def get_car(db: Session, car_id: int):
     return db.query(models.Car).filter(models.Car.id == car_id).first()
 
+
 def get_cars(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Car).offset(skip).limit(limit).all()
+
 
 def create_car(db: Session, car: schemas.CarCreate):
     db_car = models.Car(**car.dict())
@@ -18,6 +18,7 @@ def create_car(db: Session, car: schemas.CarCreate):
     db.commit()
     db.refresh(db_car)
     return db_car
+
 
 def update_car(db: Session, car_id: int, car: schemas.CarUpdate):
     db_car = get_car(db, car_id)
@@ -29,6 +30,7 @@ def update_car(db: Session, car_id: int, car: schemas.CarUpdate):
         db.refresh(db_car)
     return db_car
 
+
 def delete_car(db: Session, car_id: int):
     db_car = get_car(db, car_id)
     if db_car:
@@ -37,16 +39,14 @@ def delete_car(db: Session, car_id: int):
     return db_car
 
 
-
-
-
-
 #================================================================
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(**user.dict())
@@ -54,6 +54,7 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
     db_user = get_user(db, user_id)
@@ -64,6 +65,7 @@ def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
         db.commit()
         db.refresh(db_user)
     return db_user
+
 
 def delete_user(db: Session, user_id: int):
     db_user = get_user(db, user_id)
