1d45888691d109f5b2f395dab05ad28bfe3bd7bd
gustavo <gusgurtavo@gmail.com>
2023-12-09 16:56:37 -0300

    add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import car, user
 from .dependencies import engine
 from .models.base import Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 # Inicializa o Base com o engine para criar as tabelas no DB
 Base.metadata.create_all(bind=engine)
